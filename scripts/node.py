0d56da150c510bf9fee38b4a51ad59a41c85802d
gdamion <addlesssugar@gmail.com>
2021-01-19 21:48:54 +0300

    almost done

--- scripts/node.py
+++ scripts/node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import math as m
 import rospy
-from nav_msgs.msg import Odometry, Path, PoseStamped
+from nav_msgs.msg import Odometry, Path
+from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float64, Bool
 from kamaz_test_task.msg import KamazPath
 from kamaz_test_task.srv import StartRecord, StopRecord, ClearTrajectory
@@ -11,35 +13,52 @@ class NodeClass:
     def __init__(self):
         rospy.init_node('kamaz_test_task_node')
 
-        self.input_odom = rospy.Subscriber('/carla/ego_vehicle/odometry', Odometry, self.odomCB)
-        self.output_path = rospy.Publisher('/carla/ego_vehicle/path', KamazPath, queue_size=10)
+        self.path = Path()
+        self.path_len = float(0)
+
+        self.input = rospy.Subscriber('/carla/ego_vehicle/odometry', Odometry, self.odomCB)
+        self.output = rospy.Publisher('/path', KamazPath, queue_size=10)
         self.start_record = rospy.Service('start_record', StartRecord, self.startRecord)
         self.stop_record = rospy.Service('stop_record', StopRecord, self.stopRecord)
         self.clear_trajectory = rospy.Service('clear_trajectory', ClearTrajectory, self.clearTrajectory)
 
         self.err_req = "You've sent false request. You should send true value in the request msg!"
         self.record_started = False
-        self.path = 0.0
+        self.first_iter = True
+        self.x, self.y, self.z = float(0), float(0), float(0)
+
+        self.max_buf = rospy.get_param("~max_buf", 1000)
 
         rospy.loginfo('Node kamaz_test_task_node started')
 
-    def odomCB(self, data):
-        # self.path += 0.3
-        k_path = KamazPath()
+    def odomCB(self, new_data):
+        if self.record_started:
+            pose = PoseStamped()
+            pose.header = new_data.header
+            pose.pose = new_data.pose.pose
+            x_new, y_new, z_new = pose.pose.position.x, pose.pose.position.y, pose.pose.position.z
 
-        pose = PoseStamped()
-        pose.header.frame_id = "main"
-        pose.pose.position.x = float(data.pose.pose.position.x)
-        pose.pose.position.y = float(data.pose.pose.position.y)
-        pose.pose.position.z = float(data.pose.pose.position.z)
-        pose.pose.orientation.x = float(data.pose.pose.orientation.x)
-        pose.pose.orientation.y = float(data.pose.pose.orientation.y)
-        pose.pose.orientation.z = float(data.pose.pose.orientation.z)
-        pose.pose.orientation.w = float(data.pose.pose.orientation.w)
-        
-        # k_path.path =
-        # k_path.length =
-        self.output_path.publish()
+            if self.first_iter:
+                self.first_iter = False
+                delta_path = float(0)
+            else:
+                delta_path = m.sqrt((x_new - self.x)**2 + (y_new - self.y)**2 + (z_new - self.z)**2)
+                if self.x == x_new and self.y == y_new and self.z == z_new:
+                    return
+            self.path_len += delta_path
+            self.x, self.y, self.z = x_new, y_new, z_new
+
+            self.path.header = new_data.header
+            if (len(self.path.poses) > self.max_buf):
+                self.path.poses.pop(0)
+            self.path.poses.append(pose)
+
+        k_path = KamazPath()
+        k_path.header = new_data.header
+        k_path.path = self.path
+        k_path.path_len = Float64(self.path_len)
+
+        self.output.publish(k_path)
 
     def startRecord(self, req):
         res = StartRecordResponse()
@@ -48,6 +67,7 @@ class NodeClass:
             if not self.record_started:
                 s = "Path recording started"
                 self.record_started = True
+                self.first_iter = True
             else:
                 s = "Path recording is already started"
             res.res = s
@@ -77,9 +97,9 @@ class NodeClass:
         res = ClearTrajectoryResponse()
         if req.clear == True:
             s = ""
-            if self.path > 0.0:
+            if self.path_len > 0 and self.path.poses:
                 s = "Path recording cleared"
-                self.path = 0.0
+                self.path_len = float(0)
             else:
                 s = "Path recording is already equal to zero"
             res.res = s
